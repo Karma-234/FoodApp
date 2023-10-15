7a0f9e3533e5e850e80c6c809f2589864e149adc
Karma-234 <busari1kamal@gmail.com>
2023-10-15 11:19:54 -0700

    Initial Commit

---
+++ FoodApp/FoodAppApp.swift
@@ -0,0 +1,20 @@
+//
+//  FoodAppApp.swift
+//  FoodApp
+//
+//  Created by MAC  on 10/15/23.
+//
+
+import SwiftUI
+
+@main
+struct FoodAppApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
